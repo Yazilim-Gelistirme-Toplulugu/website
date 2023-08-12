6b824422913620074f3483d1a75feb1823e3cbd6
saziye <szy.dag2222@gmail.com>
2023-08-12 23:53:32 +0300

    Feature: Navbar logo added. Bugfix: sectionLinks text fields updated to button. Feature: aboutUs text added.

---
+++ src/app/pages/home/views/aboutUs/style.js
@@ -0,0 +1,15 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    main: {
+display: "flex",
+flexDirection: "row",
+
+    },
+
+    whoAreWeSection:{},
+    whatDoWeDoSection:{},
+    whyUsSeciton:{},
+    joinUsSection:{},
+})
+export default useStyles;
