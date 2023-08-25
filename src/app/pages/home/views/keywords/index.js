372d07b42dc57ea71c1194dbfda1cb969455882e
saziye <szy.dag2222@gmail.com>
2023-08-26 00:47:00 +0300

    Feature: Keyword animation  added.

--- src/app/pages/home/views/keywords/index.js
+++ src/app/pages/home/views/keywords/index.js
@@ -6,13 +6,70 @@ import { Button, Text, useNCoreTheme } from 'ncore-web';
 const KeywordsSection = () => {
     const { colors, spaces } = useNCoreTheme();
     const classes = useStyles();
-    const keywords = [ "HTML", "CSS", "JavaScript", "React", "React-Native", "Node.js", "No Sql", "Java", "C#", "C"];
-
+    const keywords = ["HTML", "CSS", "JavaScript", "React", "React-Native", "Node.js", "No Sql", "Java", "C#"];
+    const shuffledKeywords = keywords.sort(() => Math.random() - 0.5);
 
     return <div
         className={classes.main}
     >
-
+        <div
+            className={`${classes.keywordsContainer} ${classes.keywordsContainerLeft}`}
+        >
+            {
+                shuffledKeywords.map((item, index)=> (
+                    <div
+                        className={classes.keyword}
+                        key={index}
+                    >
+                        <Text
+                            color='body'
+                            variant='body'
+                        >
+                            {item}
+                        </Text>
+                    </div>
+                ))
+            }
+            
+        </div>
+        <div
+            className={`${classes.keywordsContainer} ${classes.keywordsContainerRight}`}
+        >
+            {
+                shuffledKeywords.map((item, index)=> (
+                    <div
+                        className={classes.keyword}
+                        key={index}
+                    >
+                        <Text
+                            color='body'
+                            variant='body'
+                        >
+                            {item}
+                        </Text>
+                    </div>
+                ))
+            }
+        </div>
+        <div
+            className={`${classes.keywordsContainer} ${classes.keywordsContainerLeft}`}
+        >
+            {
+                shuffledKeywords.map((item, index)=> (
+                    <div
+                        className={classes.keyword}
+                        key={index}
+                    >
+                        <Text
+                            color='body'
+                            variant='body'
+                        >
+                            {item}
+                        </Text>
+                    </div>
+                ))
+            }
+        </div>
     </div>
 }
 export default KeywordsSection;
--- src/app/pages/home/views/keywords/style.js
+++ src/app/pages/home/views/keywords/style.js
@@ -2,9 +2,62 @@ import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
     main: {
-        
+        display: "flex",
+        padding: "112px 10px",
+        flexDirection: "column",
+        alignItems: "center",
+        alignSelf: "stretch",
+        background: "#021524",
     },
 
-    
+    keywordsContainer: {
+        display: "flex",
+        padding: "10px",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "32px",
+        alignSelf: "stretch",
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+    },
+
+    keywordsContainerRight:{
+        animation: "$scrollRight 18s linear infinite",
+    },
+
+    keywordsContainerLeft:{
+        animation: "$scrollLeft 18s linear infinite",
+    },
+
+    keyword: {
+        display: "inline-block",
+        width: "140px",
+        padding: "18px",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        background: "#021524",
+        boxShadow: " 0px 0px 8px 0px #EF4126",
+    },
+
+    '@keyframes scrollRight': {
+        '0%': {
+            transform: 'translateX(100%)',
+        },
+        '100%': {
+            transform: 'translateX(-100%)',
+        },
+    },
+
+    '@keyframes scrollLeft': {
+        '0%': {
+            transform: 'translateX(-100%)',
+        },
+        '100%': {
+            transform: 'translateX(100%)',
+        },
+    },
+
+
 })
 export default useStyles;
